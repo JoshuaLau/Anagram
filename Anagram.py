704a810795a406c2594ebe20772993b05fc2622a
Joshua Lau <jlau37@gatech.edu>
2019-01-02 01:41:21 -0500

    Fix some names

--- Anagram.py
+++ Anagram.py
@@ -1,31 +1,30 @@
 from collections import defaultdict
-pokes = [x.strip() for x in pokes.split(",")]
 def unscramble(anagram, wordbank):
     l = []
-    for poke in wordbank:
+    for word in wordbank:
         d = defaultdict(int)
         for c in anagram.lower().replace(" ", ""):
             d[c] += 1
         check = True
-        for c in poke:
+        for c in word:
             d[c] -= 1
             if d[c] < 0:
                 check = False
                 break
         if check:
-            l.append(poke)
-    for poke in l:
+            l.append(word)
+    for word in l:
         d = defaultdict(int)
         master = defaultdict(int)
         for c in anagram.lower().replace(" ", ""):
             d[c] += 1
             master[c] += 1
-        for c in poke:
+        for c in word:
             d[c] -= 1
             master[c] -= 1
-        for p in l:
+        for word2 in l:
             check = True
-            for c in p:
+            for c in word2:
                 d[c] -= 1
                 if d[c] < 0:
                     check = False
@@ -36,9 +35,8 @@ def unscramble(anagram, wordbank):
                     if v != 0:
                         check2 = False
                 if check2:
-                    return " ".join(sorted([p, poke]))
+                    return " ".join(sorted([word2, word]))
             else:
                 for k, v in d.items():
                     d[k] = master[k]
-    return None
-
+    return None #didn't find an exact unscramble
